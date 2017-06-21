38670098877b7f458cef122425a3687a89d88755
riteeka chhabra <riteeka.chhabra@drishinfo.com>
2017-06-21 16:54:35 +0530

    commit 1.

---
+++ TestCheckoutAPP/app/src/main/java/com/testcheckoutapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testcheckoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //For initial commit
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
